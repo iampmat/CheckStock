bd11886423945a3bd78428473227f71f5aaf273f
Patrick Matherly <patrickmatherly@patricks-mbp.somers.ibm.com>
2015-06-08 09:45:07 -0400

    Initial Commit

---
+++ CheckStock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CheckStock
+//
+//  Created by Patrick Matherly on 6/8/15.
+//  Copyright (c) 2015 Patrick Matherly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
